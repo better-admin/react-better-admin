635273b5654029d08c2d2847ff2c85d879b2a4d0
开源海哥 <fuhai999@gmail.com>
2023-03-22 11:53:36 +0800

    新增开发示例

--- src/routers/modules/system.tsx
+++ src/routers/modules/system.tsx
@@ -1,13 +1,12 @@
 import {
-    DesktopOutlined,
+    BgColorsOutlined,
+    DesktopOutlined, FileDoneOutlined,
     FontSizeOutlined,
     FundOutlined,
     FundViewOutlined, GoldOutlined,
     HddOutlined,
     HighlightOutlined,
     InfoCircleOutlined,
-    MinusCircleOutlined,
-    PieChartOutlined,
     SettingOutlined,
     UnorderedListOutlined,
     UserSwitchOutlined
@@ -16,35 +15,35 @@ import lazyLoad from "../lazyLoad";
 import React, {lazy} from "react";
 import {MenuRouteObject} from "../router";
 import Page2 from "../../pages/Page2";
+import Error403 from "../../pages/Error403";
+import Error404 from "../../pages/Error404";
 
 const system: MenuRouteObject = {
     path: "system",
     label: "系统管理",
-    icon: <SettingOutlined />,
+    icon: <SettingOutlined/>,
     children: [
         {
             path: "page1",
             label: "系统参数",
-            icon: <UnorderedListOutlined />,
+            icon: <UnorderedListOutlined/>,
             element: lazyLoad(lazy(() => import("../../pages/Page1")))
         },
         {
             path: "dict",
             label: "数据字典",
-            icon: <FontSizeOutlined />,
+            icon: <FontSizeOutlined/>,
             element: <Page2/>
         },
         {
             path: "log",
             label: "日志管理",
-            icon:<InfoCircleOutlined />,
-            // element: <Page3/>,
-            // element: <CheckLogin><Portal/></CheckLogin>,
-            children:[
+            icon: <InfoCircleOutlined/>,
+            children: [
                 {
                     path: "action",
                     label: "操作日志",
-                    icon: <HighlightOutlined />,
+                    icon: <HighlightOutlined/>,
                     element: <Page2/>
                 },
                 {
@@ -57,31 +56,51 @@ const system: MenuRouteObject = {
         },
         {
             label: "系统监控",
-            icon: <FundOutlined />,
-            children:[
+            path: "monitor",
+            icon: <FundOutlined/>,
+            children: [
                 {
                     path: "users",
                     label: "在线用户",
-                    icon: <UserSwitchOutlined />,
+                    icon: <UserSwitchOutlined/>,
                     element: <Page2/>
                 },
                 {
                     path: "server",
                     label: "服务器信息",
-                    icon: <HddOutlined />,
+                    icon: <HddOutlined/>,
                     element: <Page2/>
                 },
                 {
                     path: "redis",
                     label: "Redis信息",
-                    icon: <FundViewOutlined />,
+                    icon: <FundViewOutlined/>,
                     element: <Page2/>
                 },
                 {
                     path: "caches",
                     label: "Redis缓存",
-                    icon: <GoldOutlined />,
+                    icon: <GoldOutlined/>,
                     element: <Page2/>
+                },
+            ]
+        },
+        {
+            label: "开发示例",
+            path: "dev",
+            icon: <BgColorsOutlined/>,
+            children: [
+                {
+                    path: "403",
+                    label: "403 页面",
+                    icon: <FileDoneOutlined/>,
+                    element: <Error403/>
+                },
+                {
+                    path: "404",
+                    label: "404 页面",
+                    icon: <FileDoneOutlined/>,
+                    element: <Error404/>
                 },
             ]
         }
